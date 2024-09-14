492a05aa72b570e7107c3e9080ec9163e1fc2b2c
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2024-09-14 07:03:33 -0400

    Add an Edge.prototype.show() variant, using only one line per edge

--- edge-prototype.js
+++ edge-prototype.js
@@ -19,6 +19,27 @@ Edge.prototype.show = function() {
     );
 };
 
+// For very blurry results
+if (true) {
+
+Edge.prototype.show = function() {
+    // line(this.a.pos.x, this.a.pos.y, this.b.pos.x, this.b.pos.y);
+    add3DLine(
+        this.a.pos.x, this.a.pos.y, this.a.pos.z,
+        this.b.pos.x, this.b.pos.y, this.b.pos.z,
+        1/3,
+        1, 0, 0, 0.00001
+    );
+    // add3DLine(
+    //     this.a.pos.x, this.a.pos.y, this.a.pos.z,
+    //     this.b.pos.x, this.b.pos.y, this.b.pos.z,
+    //     1/45,
+    //     1, 0, 0, 0.1
+    // );
+};
+
+}
+
 // For the blue lines
 if (false) {
 
